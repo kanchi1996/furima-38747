d7956427e7d6516845c481fa056cbe45a87a542f
kanchi1996 <amezi_1@icloud.com>
2023-07-06 18:24:02 +0900

    Active Storageの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_charge
+  belongs_to :prefecture
+  belongs_to :transport_day
+  belongs_to :user
+  has_one_attached :image
+  has_one :order
+  
 end
